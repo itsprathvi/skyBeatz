b847ee18468ef317fcbd9dedd11420dae7d97922
itsprathvi <prathvirajbellampalli@gmail.com>
2021-12-03 20:10:54 +0530

    First Commit

---
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        val bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+//        val navController = findNavController(R.id.fragmentContainerView);
+//
+//        bottomNavigationView.setupWithNavController(navController);
+
+    }
+}
