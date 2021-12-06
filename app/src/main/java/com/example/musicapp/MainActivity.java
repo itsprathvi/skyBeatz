6c521a8c9c396c5d03f19736e30518afd3b050c4
itsprathvi <prathvirajbellampalli@gmail.com>
2021-12-06 21:17:15 +0530

    Navbar Updated

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -1,22 +1,38 @@
 package com.example.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    Window window;
+    BottomNavigationView bottomNavigationView;
+    NavController navcontroller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        val bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
-//        val navController = findNavController(R.id.fragmentContainerView);
-//
-//        bottomNavigationView.setupWithNavController(navController);
+        // Navigation bar Color Change
+        if(Build.VERSION.SDK_INT>=21){
+            window = this.getWindow();
+            window.setStatusBarColor(this.getResources().getColor(R.color.black));
+        }
+        //
+
+        // Routed Fragments of Navbar
+        navcontroller = Navigation.findNavController(this, R.id.fragmentContainerView);
+        bottomNavigationView =  findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNavigationView, navcontroller);
+        //
 
     }
 }
